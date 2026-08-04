f1668e04b84159aae068481626d1f234c20797d4
afraniospohrdev <afraniorogerio@gmail.com>
2026-08-04 19:44:55 -0300

    Fix import path for main module in conftest.py
    Updated import statement to include project root in sys.path.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,15 @@
 import pytest
-from main import Base, engine   # importa direto da raiz
+import sys, os
+
+# adiciona a raiz do projeto ao sys.path
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from main import Base, engine
 
 @pytest.fixture(scope="session", autouse=True)
 def setup_database():
-    # Cria todas as tabelas antes dos testes
     Base.metadata.create_all(bind=engine)
     yield
-    # Opcional: limpar depois
     Base.metadata.drop_all(bind=engine)
+
 
