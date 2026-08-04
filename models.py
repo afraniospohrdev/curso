85fb1ea9878ff577708ce5387ce8960bd7e79174
afraniospohrdev <afraniorogerio@gmail.com>
2026-08-04 20:37:44 -0300

    Create LivroDB model for book database

---
+++ models.py
@@ -0,0 +1,10 @@
+# models.py
+from sqlalchemy import Column, Integer, String
+from db import Base
+
+class LivroDB(Base):
+    __tablename__ = "livros"
+    id = Column(Integer, primary_key=True, index=True)
+    nome_livro = Column(String, nullable=False)
+    autor_livro = Column(String, nullable=False)
+    ano_livro = Column(Integer, nullable=False)
