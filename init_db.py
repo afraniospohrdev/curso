2bdf9d2893c8f548581055ceef2f14e42647c19e
afraniospohrdev <afraniorogerio@gmail.com>
2026-08-17 20:17:14 -0300

    Log created tables after initializing database
    Added print statement to confirm created tables.

--- init_db.py
+++ init_db.py
@@ -1,6 +1,8 @@
 # init_db.py
 from db import Base, engine
-import models
+import models  # garante que LivroDB seja registrado
 
 print("Criando tabelas no banco:", engine.url)
 Base.metadata.create_all(bind=engine)
+print("Tabelas criadas:", Base.metadata.tables.keys())
+
